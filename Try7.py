9c493a5c86d9d656d92fb0cb7f7a1576afa73d88
JeffGeoffJayeff <jeffdavissecond@gmail.com>
2026-01-04 16:11:52 -0800

    New version to use point classes and hopefully be more readable

--- Try7.py
+++ Try7.py
@@ -1,5 +1,5 @@
 # The basic process of this program is Take image -> Do Scans -> Use Scans to see what x we can get
-
+# This file is refactoring everything to use point classes instead of the 
 
 from PIL import Image
 from PIL import ImageDraw
@@ -12,11 +12,11 @@ shortfile = "SteveParallel"
 InputFolder = "Input/"
 OutputFolder = "Output/"
 WorkingFolder = "Workingdir/"
-plotmatlab = False
+plotmatlab = True
 
 ReconstructionWidth = 32 #Size of the reconstruction grid, just gonna do it as a square
 Detectors = 21 #How many detectors are used
-Rotations = [0,22.5,45,67.5,90,112.5,135,157.5] #np.arange(0,181,1) # # In Degrees, angles start from the positive x-axis
+Rotations = np.arange(0,181,1) # [0,22.5,45,67.5,90,112.5,135,157.5] ## In Degrees, angles start from the positive x-axis
 
 class point:
     def __init__(self,x:float=0,y:float=0,z:float=0):
@@ -45,116 +45,110 @@ class point:
     def z(self,newz:float):
         self._z = newz
 
+    @property #Properties to make working with matrices easier
+    def xy(self):
+        return np.array([self._x,self._y])
+    @xy.setter
+    def xy(self,xy:list[float]):
+        self._x = xy[0]
+        self._y = xy[1]
+    
+    @property #3D version
+    def xyz(self):
+        return np.array([self._x,self._y,self._z])
+    @xyz.setter
+    def xyz(self,xyz:list[float]):
+        self._x = xyz[0]
+        self._y = xyz[1]
+        self._z = xyz[2]
+    def translate(self,deltax:float=0,deltay:float=0,deltaz:float=0):
+        self._x += deltax
+        self._y += deltay
+        self._z += deltaz
+    def scale(self,scalex:float=1,scaley:float=1,scalez:float=1):
+        self._x *= scalex
+        self._y *= scaley
+        self._z *= scalez
+        
 class boundingBox:
-    def __init__(self,width=1,height=1,plane=0):
-        self.boundingBoxPoints:list[point] = [point(0,0,plane),point(0,height,plane),point(width,height,plane),point(width,0,plane)] #The points that represent the four corners of the reconstruction grid, going clockwise from the bottom-left
-    @property
-    def boundingBox_BL(self): #Returns X-Y coordinates in list format for the bounding box points, this is bottom left
-        return self.boundingBoxPoints[0]
-    @property
-    def boundingBox_TL(self): #Top left
-        return self.boundingBoxPoints[1]
-    @property
-    def boundingBox_TR(self): #Top right
-        return self.boundingBoxPoints[2]
-    @property
-    def boundingBox_BR(self): #Botom Right
-        return self.boundingBoxPoints[3]
-
-
-
+    def __init__(self,width:float=1,height:float=1,plane:float=0): #The points that represent the four corners of the reconstruction grid, going clockwise from the bottom-left
+        self.boundingBox_BL = point(0,0,plane)
+        self.boundingBox_TL = point(0,height,plane)
+        self.boundingBox_TR = point(width,height,plane)
+        self.boundingBox_BR = point(width,0,plane)
+    def translate(self,deltax:float = 0,deltay:float = 0,deltaz:float=0):
+        self.boundingBox_BL.translate(deltax,deltay,deltaz)
+        self.boundingBox_TL.translate(deltax,deltay,deltaz)
+        self.boundingBox_TR.translate(deltax,deltay,deltaz)
+        self.boundingBox_BR.translate(deltax,deltay,deltaz)
+    def changeSize(self,newwidth:float=2,newheight:float=2,newplane:float=0): #Does this before moving it
+        self.boundingBox_BL.xyz = [0,0,newplane]
+        self.boundingBox_TL.xyz = [0,newheight,newplane]
+        self.boundingBox_TR.xyz = [newwidth,newheight,newplane]
+        self.boundingBox_BR.xyz = [newwidth,0,newplane]
+    def scaleAboutOrigin(self,scalex:float=1,scaley:float=1,scalez:float=1):
+        self.boundingBox_BL.scale(scalex=scalex,scaley=scaley,scalez=scalez)
+        self.boundingBox_TL.scale(scalex=scalex,scaley=scaley,scalez=scalez)
+        self.boundingBox_TR.scale(scalex=scalex,scaley=scaley,scalez=scalez)
+        self.boundingBox_BR.scale(scalex=scalex,scaley=scaley,scalez=scalez)
+    def scaleAboutPoint(self,scalex:float=1,scaley:float=1,scalez:float=1,point:point=point(0,0,0)):
+        translation = point.xyz
+        self.translate(deltax=-translation[0],
+                       deltay=-translation[1],
+                       deltaz=-translation[2])
+        self.scaleAboutOrigin(scalex=scalex,scaley=scaley,scalez=scalez)
+        self.translate(deltax=translation[0],
+                       deltay=translation[1],
+                       deltaz=translation[2])
 class Ray:
-    def __init__(self,x_1:float=-1,y_1:float=-1,x_2:float=None,y_2:float=None,length:float=0,rotationOrigin:list=[0,0]): #Two modes to do this, either define length or define points
-        self.coords = np.array([[0,0],[0,0]])
-        if length != 0:
-            self.coords = np.array([[x_1,x_1+length],[y_1,y_1]]) #This layout is probably bad practice but whatever
-        else:
-            self.coords = np.array([[x_1,x_2],[y_1,y_2]])
-        if x_2 != None:
-            self.x_2 = x_2
-            self.CalculateLength()
-        if y_2 != None:
-            self.y_2 = y_2
-            self.CalculateLength()
-        self.theta = 0 #How much the ray has been rotated from starting position
-        self.length = length
-        self.rotationOrigin = np.array([[rotationOrigin[0]],[rotationOrigin[1]]])
+    def __init__(self,point1:point = point(0,0,0),point2:point = point(1,0,0),rotationOrigin:point=point(0,0,0)): 
+        self._coords = np.array([[0,0],[0,0]])
+        self._point1:point = point1
+        self._point2:point = point2
+        self._coords = np.array([[self._point1.x,self._point2.x],[self._point1.y,self._point2.y]])
+        self._theta = 0 #How much the ray has been rotated from starting position
+        self._rotationOrigin:point = rotationOrigin
             
         #Making the rotate command like this because it will make dealing with rotations easier later
     def RotateBy(self,theta:float=0): #Rotates the ray about the center of the reconstruction grid, which is irritatingly not at 0,0
         theta = math.radians(theta)
         rotation_array = np.array([[math.cos(theta),-math.sin(theta)],[math.sin(theta),math.cos(theta)]])
-        self.coords = self.coords - self.rotationOrigin #Translation, tried +=, didn't work, this is moving the ray to be centered around the origin so the rotation matrix rotates it as expected
-        self.coords = np.matmul(rotation_array,self.coords) #Rotation
-        self.coords = self.coords + self.rotationOrigin #Translation
+        translation = np.array([[self._rotationOrigin.x],[self._rotationOrigin.y]])
+        self._coords = self._coords - translation #Translation, tried +=, didn't work, this is moving the ray to be centered around the origin so the rotation matrix rotates it as expected
+        self._coords = np.matmul(rotation_array,self._coords) #Rotation
+        self._coords = self._coords + translation #Translation
+        #TODO: Make this actually update point 1 and 2
     def RotateTo(self,desiredtheta:float=0):
-        deltatheta = desiredtheta - self.theta
+        deltatheta = desiredtheta - self._theta
         self.RotateBy(deltatheta)
-        self.theta=desiredtheta
+        self._theta=desiredtheta
     def SetTheta(self,theta:float=0): 
-        self.theta = theta
-    def CalculateLength(self):
-        deltax = abs(self.x_1-self.x_2)
-        deltay = abs(self.y_1-self.y_2)
-        self.length = math.sqrt(deltax**2+deltay**2)
-        
-    @property 
-    def x_1(self):
-        return self.coords[0,0]
-    @x_1.setter
-    def x_1(self,new:float):
-        self.coords[0,0] = new
+        self._theta = theta
         
     @property
-    def x_2(self):
-        return self.coords[0,1]
-    @x_2.setter
-    def x_2(self,new:float):
-        self.coords[0,1] = new
-        
-    @property
-    def y_1(self):
-        return self.coords[1,0]
-    @y_1.setter
-    def y_1(self,new:float):
-        self.coords[1,0] = new
-        
-    @property
-    def y_2(self):
-        return self.coords[1,1]
-    @y_2.setter
-    def y_2(self,new:float):
-        self.coords[1,1] = new
-        
-    @property
-    def x_1_float(self):
-        return self.coords[0,0]
-    @property
-    def x_2_float(self):
-        return self.coords[0,1]
-    @property
-    def y_1_float(self):
-        return self.coords[1,0]
-    @property
-    def y_2_float(self):
-        return self.coords[1,1]
+    def point1(self):
+        return self._point1
+    @point1.setter
+    def point1(self,newpoint:point):
+        self._point1 = newpoint
     
+    @property
+    def point2(self):
+        return self._point2
+    @point2.setter
+    def point2(self,newpoint:point):
+        self._point2 = newpoint
+
     def returncoords(self):
-        return [self.x_1,self.y_1,self.x_2,self.y_2]
+        return [self.point1.x,self.point1.y,self.point2.x,self.point2.y]
         
     @property
-    def rotationOrigin_x(self):
-        return self.rotationOrigin[0][0]
-    @rotationOrigin_x.setter
-    def rotationOrigin_x(self,new:float):
-        self.rotationOrigin[0][0] = new
-    
-    @property
-    def rotationOrigin_y(self):
-        return self.rotationOrigin[1][0]
-    @rotationOrigin_y.setter
-    def rotationOrigin_y(self,new:float):
-        self.rotationOrigin[1][0] = new
+    def rotationOrigin(self):
+        return self._rotationOrigin
+    @rotationOrigin.setter
+    def rotationOrigin(self,newx:float=0,newy:float=0):
+        self._rotationOrigin.x = newx
+        self._rotationOrigin.y = newy
 
 class AMatrix:
     def __init__(self):
@@ -163,14 +157,14 @@ class AMatrix:
         self.Rotations:list[float] = Rotations # Storing the rotation angles
         self.APicture:Image = Image.new("1",(self.ReconstructionWidth,self.ReconstructionWidth)) #Stores the picture that is used to make a row of A
         self.Rays: list[Ray]= [] # To store the ray objects that make up the detector
-        self.center:list[float] = [self.ReconstructionWidth/2-0.5,self.ReconstructionWidth/2-0.5] #This -0.5 is to align with the center of the pixel rather than the bottom left of a pixel
-        self.boundingBoxPoints = np.array([[0,0],[0,self.ReconstructionWidth],[self.ReconstructionWidth,self.ReconstructionWidth],[self.ReconstructionWidth,0]]) 
+        self.center:point = point(self.ReconstructionWidth/2-.5,self.ReconstructionWidth/2-0.5) #This -0.5 is to align with the center of the pixel rather than the bottom left of a pixel
+        self.boundingBoxPoints = boundingBox(self.ReconstructionWidth,ReconstructionWidth)
     #Setting Methods, should probably use the property thing but this works    
     def SetReconstruction(self,NewWidth):
         if NewWidth > 0: 
             self.ReconstructionWidth = NewWidth
             self.APicture = Image.new("1",(self.ReconstructionWidth,self.ReconstructionWidth))
-            self.center = [self.ReconstructionWidth/2-.5,self.ReconstructionWidth/2-0.5]
+            self.center = point(self.ReconstructionWidth/2-.5,self.ReconstructionWidth/2-0.5)
         else:
             print("Reconstruction grid must be larger than 0")
     def SetDetectors(self,NewDetectors):
@@ -179,11 +173,12 @@ class AMatrix:
             print(f"Now using {self.Detectors} detectors")
         else:
             print("There must be more than 0 detectors")
-    def UpdateBoundingBox(self,newWidth):
-        self.boundingBoxPoints = np.array([[0,0],[0,newWidth],[newWidth,newWidth],[newWidth,0]])
-        self.boundingBoxPoints = self.boundingBoxPoints - self.ReconstructionWidth/2 #Translating the bounding box so its centered about the origin
-        self.boundingBoxPoints = self.boundingBoxPoints*math.sqrt(2) #Scaling the bounding box about the origin by square root 2
-        self.boundingBoxPoints = self.boundingBoxPoints + self.ReconstructionWidth/2 - 0.5 #-0.5 is to align the rays with the center of pixels better
+    def UpdateBoundingBox(self,newWidth:float=2,newHeight:float=2):
+        #updates the bounding box with the assumption it should be centered about the center of the image, and scaled by square root 2
+        self.boundingBoxPoints.changeSize(newWidth,newHeight)
+        centerofImage = point(newWidth/2,newHeight/2,0)
+        self.boundingBoxPoints.scaleAboutPoint(scalex=math.sqrt(2),scaley=math.sqrt(2),point=centerofImage)
+        self.boundingBoxPoints.translate(deltax=-0.5,deltay=-0.5) #To better align the rays with the pixels
     def SetOptimalDetectors(self):
         self.SetDetectors(math.ceil(self.ReconstructionWidth*math.sqrt(2)))
     def AppendRotation(self,AdditionalRotation):
@@ -192,24 +187,22 @@ class AMatrix:
         self.Rotations.extend(AdditionalRotations)
     # Ray creation methods
     def CreateParallelRays(self): #Makes the rays parallel
-        self.UpdateBoundingBox(self.ReconstructionWidth)
-        rayGroupWidth = self.ReconstructionWidth*math.sqrt(2) #Multiplying by the square root two to get the diagonal width
-        raySpacing = (self.boundingBox_TL[1]-self.boundingBox_BL[1])/(self.Detectors-1)#Lin space forumala
-        xCoordinate = self.boundingBox_BL[0]#(self.ReconstructionWidth-rayGroupWidth)/2+0.5#self.ReconstructionWidth/2*(1-math.sqrt(2))# #Same for all rays
+        self.UpdateBoundingBox(self.ReconstructionWidth,self.ReconstructionWidth) #TODO: Allow for the possibility of non-square bounding box
+        raySpacing = (self.boundingBoxPoints.boundingBox_TL.y-self.boundingBoxPoints.boundingBox_BL.y)/(self.Detectors-1)#Lin space forumala
         for i in range(0,self.Detectors):
             #debug statement print(f"i: {i} | xCoodrinate: {xCoordinate} | yCoordinate: {xCoordinate + raySpacing*i}")
-            self.Rays.append(Ray(x_1=xCoordinate,
-                                 y_1=xCoordinate+raySpacing*i,
-                                 length=rayGroupWidth,
+            ycoordinate = self.boundingBoxPoints.boundingBox_BL.y+raySpacing*i
+            self.Rays.append(Ray(point1=point(self.boundingBoxPoints.boundingBox_BL.x,ycoordinate),
+                                 point2=point(self.boundingBoxPoints.boundingBox_BR.x,ycoordinate),
                                  rotationOrigin=self.center))
     def CreateFanRays(self): #Makes the rays into a fan pattern
         self.UpdateBoundingBox(self.ReconstructionWidth)
-        raySpacing = (self.boundingBox_TL[1]-self.boundingBox_BL[1])/(self.Detectors-1)
-        fanningPointY = self.center[1] # Y coordinate of the points where the rays fan from
-        fanningPointX = self.boundingBox_BR[0]
-        fanWallx = self.boundingBox_BL[0]
+        raySpacing = (self.boundingBoxPoints.boundingBox_TL.y-self.boundingBoxPoints.boundingBox_BL.y)/(self.Detectors-1)
+        fanningPointY = self.center.y # Y coordinate of the points where the rays fan from
+        fanningPointX = self.boundingBoxPoints.boundingBox_BR.x
+        fanWallx = self.boundingBoxPoints.boundingBox_BL.x
         for i in range(0,self.Detectors):
-            ycoord = self.boundingBox_BL[1]+i*raySpacing
+            ycoord = self.boundingBoxPoints.boundingBox_BL.y+i*raySpacing
             self.Rays.append(Ray(x_1=fanningPointX,
                                  y_1=fanningPointY,
                                  x_2=fanWallx,
@@ -271,21 +264,16 @@ class AMatrix:
         print("R Matrix created")
     # Debugging
     def DebugCreateX(self): 
-        self.Rays.append(Ray(x_1=self.boundingBox_BL[0],x_2=self.boundingBox_TR[0],y_1=self.boundingBox_BL[1],y_2=self.boundingBox_TR[1],rotationOrigin=self.center))
-        self.Rays.append(Ray(x_1=self.boundingBox_TL[0],x_2=self.boundingBox_BR[0],y_1=self.boundingBox_TL[1],y_2=self.boundingBox_BR[1],rotationOrigin=self.center))
-    # Properties
-    @property
-    def boundingBox_BL(self): #Returns X-Y coordinates in list format for the bounding box points, this is bottom left
-        return self.boundingBoxPoints[0]
-    @property
-    def boundingBox_TL(self): #Top left
-        return self.boundingBoxPoints[1]
-    @property
-    def boundingBox_TR(self): #Top right
-        return self.boundingBoxPoints[2]
-    @property
-    def boundingBox_BR(self): #Botom Right
-        return self.boundingBoxPoints[3]
+        self.Rays.append(Ray(x_1=self.boundingBoxPoints.boundingBox_BL.x,
+                             x_2=self.boundingBoxPoints.boundingBox_TR.x,
+                             y_1=self.boundingBoxPoints.boundingBox_BL.y,
+                             y_2=self.boundingBoxPoints.boundingBox_TR.y,
+                             rotationOrigin=self.center))
+        self.Rays.append(Ray(x_1=self.boundingBoxPoints.boundingBox_TL.x,
+                             x_2=self.boundingBoxPoints.boundingBox_BR.x,
+                             y_1=self.boundingBoxPoints.boundingBox_TL.y,
+                             y_2=self.boundingBoxPoints.boundingBox_BR.y,
+                             rotationOrigin=self.center))
 class xMatrix: #This should be done after the AMatrix is made, manages everything related to the constructed xMatrix
     def __init__(self,Amatrix:AMatrix,ReconWidth:float=3):
         self.ReconstructionWidth = ReconWidth
@@ -336,7 +324,6 @@ class xMatrix: #This should be done after the AMatrix is made, manages everythin
         print(f"Final image saved!")
     #Saving Stuff Section
     def SaveImage(self,filename): #Function to save the image and add it to frames to make a gif later
-        print(self.xarray)
         output = np.reshape(self.xarray.astype(np.int8),(self.ReconstructionWidth,self.ReconstructionWidth))
         max = np.max(output)
         if max > 0:
@@ -365,7 +352,7 @@ def MakeSinogram(inputarray,filename,numberofrotations,detectors):
 def PlotAMatrix(AMatrix: AMatrix, ax: plt.axes):
     rays: list[Ray] = AMatrix.Rays
     for i in rays:
-        ax.plot([i.x_1,i.x_2],[i.y_1,i.y_2])
+        ax.plot([i.point1.x,i.point2.x],[i.point1.y,i.point2.y])
 def main():
     try:
         path = "Input/"+filename
@@ -392,8 +379,8 @@ def main():
             ax.imshow(img)
             PlotAMatrix(thedog,ax)
             thedog.RotateRaysTo(i)
-            ax.set_xlim([thedog.boundingBox_BL[0]-5,thedog.boundingBox_BR[0]+5])
-            ax.set_ylim([thedog.boundingBox_BL[1]-5,thedog.boundingBox_TL[1]+5])  
+            ax.set_xlim([thedog.boundingBoxPoints.boundingBox_BL.x-5,thedog.boundingBoxPoints.boundingBox_BR.x+5])
+            ax.set_ylim([thedog.boundingBoxPoints.boundingBox_BL.y-5,thedog.boundingBoxPoints.boundingBox_TL.y+5])  
             
             plt.savefig(f"Workingdir/Torture/{i}.png")
             ax.cla()   
